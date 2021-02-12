3c051345631e2b428b52aa773a1aff138fb15e7d
Markus <31160708+markusschmitt@users.noreply.github.com>
2021-02-12 15:18:44 +0100

    More doc.

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 sys.path.append(os.path.abspath("../../"))
  
-MOCK_MODULES = ['mpi4py']
+MOCK_MODULES = ['mpi4py','flax']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
