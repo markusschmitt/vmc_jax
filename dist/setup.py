890d6324ae889342ad967f5dff41f2a445b95ff3
mr128254 <moritz.reh@gmail.com>
2021-06-23 16:46:23 +0200

    changed long description of package

--- dist/setup.py
+++ dist/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     author="Markus Schmitt",
     author_email="markus.schmitt@uni-koeln.de",
     description="jVMC: Versatile and performant variational Monte Carlo",
-    long_description="This package, available on GitHub, provides a versatile and efficient implementation of variational quantum Monte Carlo in Python. It utilizes Google’s JAX library to exploit the blessings of automatic differentiation and just-in-time compilation for available computing resources. The package is devised to serve as white box implementation of the core computational tasks that guarantees efficiency, while at the same time providing large flexibility.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://jvmc.readthedocs.io/en/latest/#",
     packages=setuptools.find_packages(),
