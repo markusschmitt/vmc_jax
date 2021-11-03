61c40d1d369c3603bbe1eab57218dc8a371a7b22
markusschmitt <31160708+markusschmitt@users.noreply.github.com>
2021-11-03 16:05:52 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='jVMC',
-    version='0.1.0',
+    version='0.1.1',
     author="Markus Schmitt, Moritz Reh",
     author_email="markus.schmitt@uni-koeln.de",
     description="jVMC: Versatile and performant variational Monte Carlo",
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://jvmc.readthedocs.io/en/latest/#",
     packages=setuptools.find_packages(),
-    install_requires=["setuptools", "wheel", "numpy", "jax", "jaxlib", "flax", "mpi4py", "h5py"],
+    install_requires=["setuptools", "wheel", "numpy", "jax==0.2.24", "jaxlib==0.1.73", "flax==0.3.6", "mpi4py", "h5py"],
     classifiers=[
         "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
