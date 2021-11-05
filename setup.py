5a27793eeb5b749fe1f2658bc006d01891205eaa
markusschmitt <31160708+markusschmitt@users.noreply.github.com>
2021-11-05 12:00:18 +0100

    Relaxed dependency restrictions.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+DEFAULT_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax>=0.2.21,<=0.2.25", "jaxlib>=0.1.71,<=0.1.73", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
+CUDA_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax[cuda]>=0.2.21,<=0.2.25", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
+
 setuptools.setup(
     name='jVMC',
     version='0.1.1',
@@ -14,7 +17,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://jvmc.readthedocs.io/en/latest/#",
     packages=setuptools.find_packages(),
-    install_requires=["setuptools", "wheel", "numpy", "jax==0.2.24", "jaxlib==0.1.73", "flax==0.3.6", "mpi4py", "h5py"],
+    install_requires=DEFAULT_DEPENDENCIES,
+    extras_require={
+        "cuda": CUDA_DEPENDENCIES
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
