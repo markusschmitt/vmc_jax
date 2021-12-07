07d427413eb7a13f8a5cc196c57e1c7cf6ac4f7d
markusschmitt <31160708+markusschmitt@users.noreply.github.com>
2021-12-07 13:36:51 +0100

    Modified setup.py to remove [cuda].

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-DEFAULT_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax>=0.2.21,<=0.2.25", "jaxlib>=0.1.71,<=0.1.74", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
-CUDA_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax[cuda]>=0.2.21,<=0.2.25", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
+DEFAULT_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax>=0.2.11,<=0.2.25", "jaxlib>=0.1.71,<=0.1.74", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
+#CUDA_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax[cuda]>=0.2.11,<=0.2.25", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
 
 setuptools.setup(
     name='jVMC',
@@ -18,9 +18,9 @@ setuptools.setup(
     url="https://jvmc.readthedocs.io/en/latest/#",
     packages=setuptools.find_packages(),
     install_requires=DEFAULT_DEPENDENCIES,
-    extras_require={
-        "cuda": CUDA_DEPENDENCIES
-    },
+#    extras_require={
+#        "cuda": CUDA_DEPENDENCIES
+#    },
     classifiers=[
         "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
