a353a34401087d72aaf79de87d2238036a9a33f7
markusschmitt <31160708+markusschmitt@users.noreply.github.com>
2022-01-11 11:03:20 +0100

    Fixed required version number for jaxlib.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-DEFAULT_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax>=0.2.11,<=0.2.25", "jaxlib>=0.1.71,<=0.1.74", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
+DEFAULT_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax>=0.2.11,<=0.2.25", "jaxlib>=0.1.64,<=0.1.74", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
 #CUDA_DEPENDENCIES = ["setuptools", "wheel", "numpy", "jax[cuda]>=0.2.11,<=0.2.25", "flax>=0.3.6,<=0.3.6", "mpi4py", "h5py"]
 
 setuptools.setup(
     name='jVMC',
-    version='0.1.2',
+    version='0.1.3',
     author="Markus Schmitt, Moritz Reh",
     author_email="markus.schmitt@uni-koeln.de",
     description="jVMC: Versatile and performant variational Monte Carlo",
