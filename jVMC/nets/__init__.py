33d3071a1d93849e6171f4a9f597cdac08ad731f
Markus <markus.schmitt@uni-koeln.de>
2022-03-04 09:34:47 +0100

    Cleanup

--- jVMC/nets/__init__.py
+++ jVMC/nets/__init__.py
@@ -2,8 +2,5 @@ from jVMC.nets.activation_functions import *
 from jVMC.nets.rbm import *
 from jVMC.nets.cnn import *
 from jVMC.nets.ffn import *
-from jVMC.nets.rnn import *
-from jVMC.nets.rnn2d import *
 from jVMC.nets.rnn1d_general import *
 from jVMC.nets.rnn2d_general import *
-from jVMC.nets.lstm import *
