d362340b606ff71f72afede7a0ab5dd05bf48af7
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2022-07-11 15:09:13 +0200

    extended POVM compile to three body interactions

--- jVMC/operator/povm.py
+++ jVMC/operator/povm.py
@@ -357,12 +357,15 @@ class POVMOperator(Operator):
     def _get_s_primes(self, s, *args):
         def apply_on_singleSiteCoupling(s, stateCouplings, matEls, siteCoupling):
             stateIndices = tuple(s[siteCoupling])
-            OffdConfig = jnp.vstack([s] * 16)
-            OffdConfig = OffdConfig.at[:, siteCoupling].set(stateCouplings[stateIndices].reshape(-1, 2))
-            return OffdConfig.reshape((4, 4, -1)), matEls[stateIndices]
+            OffdConfig = jnp.vstack([s] * 4**3)
+            OffdConfig = OffdConfig.at[:, siteCoupling].set(stateCouplings[stateIndices].reshape(4**3, 3))
+            return OffdConfig.reshape((4, 4, 4, 3)), matEls[stateIndices]
 
         sample_shape = s.shape
-        OffdConfigs, matEls = jax.vmap(apply_on_singleSiteCoupling, in_axes=(None, None, 0, 0))(s.reshape(-1), self.stateCouplings, self.matEls, self.siteCouplings)
+        OffdConfigs, matEls = jax.vmap(apply_on_singleSiteCoupling, in_axes=(None, None, 0, 0))(s.reshape(-1),
+                                                                                                self.stateCouplings,
+                                                                                                self.matEls,
+                                                                                                self.siteCouplings)
         return OffdConfigs.reshape((-1,) + s.shape), matEls.reshape(-1)
 
     def compile(self):
@@ -370,44 +373,95 @@ class POVMOperator(Operator):
         """
         self.siteCouplings = []
         self.matEls = []
-        self.idxbase = jnp.array([[[i, j] for j in range(4)] for i in range(4)])
-        self.stateCouplings = jnp.array([[self.idxbase for j in range(4)] for i in range(4)])
+        self.idxbase = jnp.array([[[[i, j, k] for k in range(4)] for j in range(4)] for i in range(4)])
+        self.stateCouplings = jnp.array([[[self.idxbase for j in range(4)] for i in range(4)] for k in range(4)])
 
         # find the highest index of the (many) local Hilbert spaces
         self.max_site = max([max(op["sites"]) for op in self.ops])
 
         # loop over all local Hilbert spaces
         for idx in range(self.max_site + 1):
-            # sort interactions that involve the current local Hilbert space in the 0-th index into one- and two-body interactions
+            # sort interactions that involve the current local Hilbert space in the 0-th index into one-, two- and
+            # three-body interactions
             ops_oneBody = [op for op in self.ops if op["sites"][0] == idx and len(op["sites"]) == 1]
             ops_twoBody = [op for op in self.ops if op["sites"][0] == idx and len(op["sites"]) == 2]
+            ops_threeBody = [op for op in self.ops if op["sites"][0] == idx and len(op["sites"]) == 3]
 
             # find all the local Hilbert spaces that are coupled to the one we currently selected ("idx")
-            neighbour_indices = set([op["sites"][1] for op in ops_twoBody])
-            if len(ops_twoBody) == 0 and len(ops_oneBody) > 0:
-                # the current local Hilbert space is not listed in the 0-th index of any of the two body interactions, but 1-body interactions still need to respected
-                # create artificial coupling to a second spin with a unity operator acting on spin #2
-                neighbour_op_comp = jnp.zeros((16, 16), dtype=opDtype)
-                for op_oneBody in ops_oneBody:
-                    neighbour_op_comp += op_oneBody["strength"] * jnp.kron(self.povm.operators[op_oneBody["name"]], jnp.eye(4, dtype=opDtype))
-
-                self.matEls.append(neighbour_op_comp.reshape((4,) * 4))
-                self.siteCouplings.append([idx, (idx + 1) % (self.max_site + 1)])
-            else:
-                for neighbour_idx in neighbour_indices:
-                    # get all the operators that include the neighbour index
-                    neighbour_ops = [op for op in ops_twoBody if op["sites"][1] == neighbour_idx]
-
-                    # obtain 16x16 matrices for these operators and add the single particle  operators with weight 1 / # of neighbours
-                    neighbour_op_comp = jnp.zeros((16, 16), dtype=opDtype)
-                    for neighbour_op in neighbour_ops:
-                        neighbour_op_comp += neighbour_op["strength"] * self.povm.operators[neighbour_op["name"]]
+            if len(ops_threeBody) == 0:
+                neighbour_indices = set([op["sites"][1] for op in ops_twoBody])
+                if len(ops_twoBody) == 0 and len(ops_oneBody) > 0:
+                    # the current local Hilbert space is not listed in the 0-th index of any of the two-body
+                    # interactions, but 1-body interactions still need to be respected
+                    # create artificial coupling to a second and third spin with unity operators acting on spin #2 and #3
+                    neighbour_op_comp = jnp.zeros((4**3, 4**3), dtype=opDtype)
                     for op_oneBody in ops_oneBody:
-                        neighbour_op_comp += op_oneBody["strength"] * jnp.kron(self.povm.operators[op_oneBody["name"]], jnp.eye(4, dtype=opDtype)) / len(neighbour_indices)
+                        neighbour_op_comp += op_oneBody["strength"] * jnp.kron(self.povm.operators[op_oneBody["name"]],
+                                                                               jnp.kron(jnp.eye(4, dtype=opDtype),
+                                                                                        jnp.eye(4, dtype=opDtype)))
 
-                    # for the computed operator (16x16) obtain a representation in which a matrix element and the connected indices are stored
-                    self.matEls.append(neighbour_op_comp.reshape((4,) * 4))
-                    self.siteCouplings.append([idx, neighbour_idx])
+                    self.matEls.append(neighbour_op_comp.reshape((4,) * 2*3))
+                    self.siteCouplings.append([idx, (idx + 1) % (self.max_site + 1),  (idx + 2) % (self.max_site + 1)])
+                else:
+                    for neighbour_idx in neighbour_indices:
+                        # get all the operators that include the neighbour index
+                        neighbour_ops = [op for op in ops_twoBody if op["sites"][1] == neighbour_idx]
+
+                        # obtain 64x64 matrices for these operators and add the single particle operators with weight 1 / # of neighbours
+                        neighbour_op_comp = jnp.zeros((4**3, 4**3), dtype=opDtype)
+                        for neighbour_op in neighbour_ops:
+                            neighbour_op_comp += neighbour_op["strength"] * jnp.kron(self.povm.operators[neighbour_op["name"]],
+                                                                                     jnp.eye(4, dtype=opDtype))
+                        for op_oneBody in ops_oneBody:
+                            neighbour_op_comp += op_oneBody["strength"] * jnp.kron(self.povm.operators[op_oneBody["name"]],
+                                                                                   jnp.kron(jnp.eye(4, dtype=opDtype),
+                                                                                            jnp.eye(4, dtype=opDtype)))\
+                                                 / len(neighbour_indices)
+
+                        # for the computed operator (64x64) obtain a representation in which a matrix element and the connected indices are stored
+                        self.matEls.append(neighbour_op_comp.reshape((4,) * 2*3))
+
+                        # make sure that the third index is not the same as the second index
+                        empty_idx = (idx + 1) % (self.max_site + 1)
+                        if neighbour_idx == empty_idx:
+                            empty_idx = (idx + 2) % (self.max_site + 1)
+                        self.siteCouplings.append([idx, neighbour_idx, empty_idx])
+            else:
+                neighbour_indices1 = set([op["sites"][1] for op in ops_threeBody])
+                neighbour_indices2 = set([op["sites"][2] for op in ops_threeBody])
+                for neighbour_idx1 in neighbour_indices1:
+                    for neighbour_idx2 in neighbour_indices2:
+                        neighbour_ops = [op for op in ops_threeBody if op["sites"][1] == neighbour_idx1 and
+                                         op["sites"][2] == neighbour_idx2]
+
+                        neighbour_op_comp = jnp.zeros((4**3, 4**3), dtype=opDtype)
+                        for neighbour_op in neighbour_ops:
+                            neighbour_op_comp += neighbour_op["strength"] * self.povm.operators[neighbour_op["name"]]
+                        for op_oneBody in ops_oneBody:
+                            neighbour_op_comp += op_oneBody["strength"] * jnp.kron(self.povm.operators[op_oneBody["name"]],
+                                                                                   jnp.kron(jnp.eye(4, dtype=opDtype),
+                                                                                            jnp.eye(4, dtype=opDtype)))\
+                                                 / len(neighbour_indices1) / len(neighbour_indices2)
+
+                        self.matEls.append(neighbour_op_comp.reshape((4,) * 2*3))
+                        self.siteCouplings.append([idx, neighbour_idx1, neighbour_idx2])
+
+                # deal with two-body interactions separately since the second spin index may not be the same as for
+                # the three-body interactions
+                neighbour_indices3 = set([op["sites"][1] for op in ops_twoBody])
+                for neighbour_idx3 in neighbour_indices3:
+                    neighbour_ops = [op for op in ops_twoBody if op["sites"][1] == neighbour_idx3]
+
+                    neighbour_op_comp = jnp.zeros((4**3, 4**3), dtype=opDtype)
+                    for neighbour_op in neighbour_ops:
+                        neighbour_op_comp += neighbour_op["strength"] * jnp.kron(self.povm.operators[neighbour_op["name"]],
+                                                                                 jnp.eye(4, dtype=opDtype))
+
+                    empty_idx = (idx + 1) % (self.max_site + 1)
+                    if neighbour_idx == empty_idx:
+                        empty_idx = (idx + 2) % (self.max_site + 1)
+                    self.matEls.append(neighbour_op_comp.reshape((4,) * 2*3))
+                    self.siteCouplings.append([idx, neighbour_idx3, empty_idx])
 
         self.siteCouplings = jnp.array(self.siteCouplings)
         self.matEls = jnp.array(self.matEls)
