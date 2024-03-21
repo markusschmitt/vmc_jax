e7c6dd7550f1889e1ee177e6cdfec3fa22a26f83
j.rigo <j.rigo@fz-juelich.de>
2024-03-21 11:39:20 +0100

    added functionality to save the pytree parameter dictionary to hdf5

--- jVMC/util/__init__.py
+++ jVMC/util/__init__.py
@@ -3,4 +3,5 @@ from jVMC.util.minsr import *
 from jVMC.util.stepper import *
 from jVMC.util.util import *
 from jVMC.util.output_manager import *
+from jVMC.util.Parameters2HDF5 import *
 import jVMC.util.symmetries
