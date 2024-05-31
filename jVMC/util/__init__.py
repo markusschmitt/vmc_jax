c831298eec7f02a08230c54bbe1eea7b6c901bed
Jonas Rigo <47185182+JonasRigo@users.noreply.github.com>
2024-05-31 11:41:34 -0400

    Reverted __init__.py to before adding Parameters2HDF5

--- jVMC/util/__init__.py
+++ jVMC/util/__init__.py
@@ -3,5 +3,4 @@ from jVMC.util.minsr import *
 from jVMC.util.stepper import *
 from jVMC.util.util import *
 from jVMC.util.output_manager import *
-from jVMC.util.Parameters2HDF5 import *
 import jVMC.util.symmetries
